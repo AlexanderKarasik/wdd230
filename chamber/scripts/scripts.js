c0d87c6b873545c92ceb222c8bea011e255e56db
AlexanderKarasik <yakarasik@yandex.ru>
2022-10-19 00:18:42 +0300

    updates

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,8 +1,8 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
 }
-const x = document.getElementById('hamburgerBtn')
+const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
 const d = new Date();
@@ -25,30 +25,40 @@ document.getElementById("time").innerHTML =
   lastmodif.toLocaleTimeString("en-US");
 
 document.getElementById("lastupdate1").innerHTML = lastmodif.toLocaleDateString(
-"en-US",
-options
+  "en-US",
+  options
 );
 document.getElementById("time1").innerHTML =
-lastmodif.toLocaleTimeString("en-US");
+  lastmodif.toLocaleTimeString("en-US");
 
 document.getElementById("lastupdate2").innerHTML = lastmodif.toLocaleDateString(
-    "en-US",
-    options
-  );
-  document.getElementById("time2").innerHTML =
-    lastmodif.toLocaleTimeString("en-US");
+  "en-US",
+  options
+);
+document.getElementById("time2").innerHTML =
+  lastmodif.toLocaleTimeString("en-US");
 
 // Day today with week day name
 
 const date1 = document.querySelector("#today");
 try {
-	const options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-		year: "numeric"
-	};
-	date1.innerHTML = `<span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>`;
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  date1.innerHTML = `<span class="highlight">${new Date().toLocaleDateString(
+    "en-UK",
+    options
+  )}</span>`;
 } catch (e) {
-	alert("Error with code or your browser does not support Locale");
+  alert("Error with code or your browser does not support Locale");
+}
+
+// TEMPORAL BANNER
+if (new Date().getDay() == 1 || new Date().getDay() == 2) {
+  document.querySelector("#bannertemp").style.display = "block";
+} else {
+  document.querySelector("#bannertemp").style.display = "none";
 }
