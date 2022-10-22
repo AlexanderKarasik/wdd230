0247107f34d69c9fd05e85ae75d43b0ec1549a67
AlexanderKarasik <yakarasik@yandex.ru>
2022-10-23 01:13:46 +0300

    updates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -10,10 +10,11 @@
 
 // console.log(temperature);
 // console.log(s);
+function calcchill(){
+let t = document.getElementById("theInputTemp").value*1.8+32;
+let s = document.getElementById("windspeed").value/1.609;
 
-let t = document.getElementById("theInputTemp").innerText*1.8+32;
-let s = document.getElementById("windspeed").innerText/1.609;
- console.log(s);
+console.log(t);
 
 let windchill_fahr = 35.74 + 0.6215*t - 35.75*(s**0.16) + 0.4275*t*(s**0.16);
 let windchill_cels = (windchill_fahr - 32)*5/9;
@@ -21,6 +22,6 @@ if (t<=50 && s>3){
     document.querySelector('.windchill').innerHTML = Math.round(windchill_cels) + ' ℃';
 } 
     else{
-        document.querySelector('.windchill').innerHTML = 'N/A';
+        document.querySelector('.windchill').innerHTML = ' N/A';
     }
-
+}
