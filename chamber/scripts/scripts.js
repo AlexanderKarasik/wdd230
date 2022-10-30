e71cc168e33b3e64b6f3ccab9d9278d4ff170949
AlexanderKarasik <yakarasik@yandex.ru>
2022-10-31 00:21:38 +0300

    new

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -66,33 +66,8 @@ try {
 // TEMPORAL BANNER
 console.log(new Date().getDay());
 if (new Date().getDay() == 1 || new Date().getDay() == 2) {
-  document.querySelector("#bannertemp").style.display = "block";
-} else {
-  document.querySelector("#bannertemp").style.display = "none";
-}
+  document.querySelector("#bannertemp").style.display = "block";} 
 
-// DISCOVER, LAST VISIT
-let lastVisit = Number(window.localStorage.getItem("lastVisitDate"));
-if (lastVisit !== 0) {
-  let date_1 = lastVisit;
-  let date_2 = Date.now();
-  let difference = date_2 - date_1;
-  let result = Math.floor(difference / (1000 * 3600 * 24));
-  if (result === 0) {
-    document.querySelector("#lastVisit").textContent =
-      "You have visited this page less than one day ago.";
-  } else if (result === 1) {
-    document.querySelector("#lastVisit").textContent =
-      "You have visited this page one day ago.";
-  } else {
-    document.querySelector(
-      "#lastVisit"
-    ).textContent = `You have visited this page ${result} days ago.`;
-  }
-} else {
-  result = "This is your first visit.";
-  document.querySelector("#lastVisit").textContent = result;
-}
-localStorage.setItem("lastVisitDate", Date.now());
+
 
 
